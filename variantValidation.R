c657313012f6ac93388ad7c3374b7610396597d1
lanieehapp <lanieehapp@gmail.com>
2020-07-14 10:10:52 -0400

    merged RNA_filt with filt.gt

--- variantValidation.R
+++ variantValidation.R
@@ -29,7 +29,7 @@ ref<-args[[4]]
 load(vcf_path)
 
 filt.fix<-single.sample.merged[[4]]
-filt.gt<-single.sample.merged[[5]]
+filt.gt<-single.sample.merged[[6]]
 
 var.length<-apply(filt.fix[,4:5], 1, function(x){max(nchar(x))})
 end.pos<-as.numeric(filt.fix$POS)+var.length-1
@@ -42,7 +42,7 @@ file<-dna_bam_path
 #file<-paste0("/data/", file)
 
 #comm<-paste0("bcftools mpileup -R whitelist_bed.txt -d 1000 --fasta-ref GRCh38.p12.genome.plus.ERCC.fa ", file, " > rna_vars.txt" )
-comm<-paste0('bcftools mpileup -R whitelist_bed.txt -d 1000 --fasta-ref ',ref, ' ', file, ' | bcftools call -m -A -M | grep -v "##" > dna_whitelist.txt' )
+comm<-paste0('bcftools mpileup -R whitelist_bed.txt -d 1000 --ignore-RG --fasta-ref ',ref, ' ', file, ' | bcftools call -m -A -M | grep -v "##" > dna_whitelist.txt' )
 system(comm)
 
 dna_whitelist<-read.csv(file="dna_whitelist.txt", sep="\t", stringsAsFactors = FALSE)
@@ -88,7 +88,7 @@ file<-rna_bam_path
 #file<-paste0("/", file)
 
 #comm<-paste0("bcftools mpileup -R whitelist_bed.txt -d 1000 --fasta-ref GRCh38.p12.genome.plus.ERCC.fa ", file, " > rna_vars.txt" )
-comm<-paste0('bcftools mpileup -R whitelist_bed.txt -d 1000 --fasta-ref ', ref, ' ', file, ' | bcftools call -m -A -M | grep -v "##" > rna_whitelist.txt' )
+comm<-paste0('bcftools mpileup -R whitelist_bed.txt -d 1000 --ignore-RG --fasta-ref ', ref, ' ', file, ' | bcftools call -m -A -M | grep -v "##" > rna_whitelist.txt' )
 system(comm)
 
 rna_whitelist<-read.csv(file="rna_whitelist.txt", sep="\t", stringsAsFactors = FALSE)
@@ -134,7 +134,7 @@ all_whitelist<-merge(x=whitelist, y=whitelist_rna, by="CHROM_POS_REF_ALT", all.x
 ##################### dna filtered variants in rna ####################
 
 #comm<-paste0("bcftools mpileup -R whitelist_bed.txt -d 1000 --fasta-ref GRCh38.p12.genome.plus.ERCC.fa ", file, " > rna_vars.txt" )
-comm<-paste0('bcftools mpileup -R filt_bed.txt -d 1000 --fasta-ref ', ref, " ", file, ' | bcftools call -m -A -M | grep -v "##" > /data/rna_filt.txt' )
+comm<-paste0('bcftools mpileup -R filt_bed.txt -d 1000 --ignore-RG --fasta-ref ', ref, " ", file, ' | bcftools call -m -A -M | grep -v "##" > /data/rna_filt.txt' )
 system(comm)
 
 rna_filt<-read.csv(file="/data/rna_filt.txt", sep="\t", stringsAsFactors = FALSE)
@@ -172,18 +172,34 @@ filt_rna$RNA_EVIDENCE[filt_rna$RNA_DEPTH<30]<-"LOW_COVERAGE"
 filt_rna$RNA_EVIDENCE[dp4$X3>1 & dp4$X4>1]<-TRUE
 filt_rna$RNA_EVIDENCE[str_count(rna_filt$ALT, ",")>0]<-"MULTIALLELIC"
 
+filt_rna$CHROM_POS<-paste0(rna_filt$X.CHROM, "-", rna_filt$POS)
+filt_rna$ALT<-rna_filt$ALT
+
+fix_pos<-paste0(filt.fix$CHROM, "-", filt.fix$POS)
+
+filt.gt$RNA_EVIDENCE<-NA
 for(i in 1:nrow(filt_rna)){
-  var<-as.character(filt_rna$CHROM_POS_REF_ALT[i])
-  var_spl<-unlist(str_split(var, "-"))
-  if(paste0(var_spl[1], "-", var_spl[2]) %in% paste0(filt.fix$CHROM, "-", filt.fix$POS)){
-    
+  if(filt_rna$CHROM_POS[i] %in% fix_pos){
+    idx<-which(fix_pos == filt_rna$CHROM_POS[i])
+    filt.gt$RNA_EVIDENCE[idx]<-filt_rna$RNA_EVIDENCE[i]
   }
-  
 }
+
+samp.id<-unlist(str_split(colnames(filt.gt)[2], "[.]"))[1]
+
+colnames(filt.gt)[36]<-paste0(samp.id, ".RNA_EVIDENCE")
 
 #all_whitelist<-merge(x=whitelist, y=whitelist_rna, by="CHROM_POS_REF_ALT", all.x=T, all.y=T)
 
+single.sample.merged[[6]]<-filt.gt
 
+single.sample.filt<-list(single.sample.merged[[4]], single.sample.merged[[5]], filt.gt)
 
-write.table(filt_rna, file="/data/filtered_vars_in_RNA.tsv", quote=FALSE, sep="\t", row.names = FALSE)
+save(single.sample.filt, file="/data/single_sample_filtered_withRNA.RData")
+#write.table(filt_rna, file="/data/filtered_vars_in_RNA.tsv", quote=FALSE, sep="\t", row.names = FALSE)
 write.table(all_whitelist, file="/data/all_whitelist_vars.tsv", quote=FALSE, sep="\t", row.names=FALSE)
+
+
+simple.single.sample<-cbind(filt.fix, filt.gt[,grep("nCallers|afMax|dpMax|RNA_EVIDENCE", colnames(filt.gt))])
+write.table(simple.single.sample, file="/data/simple_single_sample.tsv", quotes=FALSE, row.names = FALSE, sep="\t")
+
